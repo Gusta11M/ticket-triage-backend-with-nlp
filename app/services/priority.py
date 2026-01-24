fba7c14f42684ff0ee36ac36ef79be9986b09933
Gusta11M <gustadaniel.marques@gmail.com>
2026-01-24 21:39:23 +0000

    feat: implement priority service with CRUD operations

---
+++ app/services/priority.py
@@ -0,0 +1,24 @@
+from app.models.priority import Priority
+from app.schemas.priority import PriorityCreateSchema
+from app.repositories.priority_repository import create_priority, delete_priority, get_priority_by_id, get_all_priorities, update_priority
+from sqlalchemy.ext.asyncio import AsyncSession
+
+async def create_priority(db:AsyncSession, priority:PriorityCreateSchema) -> Priority:
+    priority_obj = await create_priority(db, priority)
+    return priority_obj
+
+async def get_priority_service(db:AsyncSession, priority_id:int) -> Priority | None:
+    priority_obj = await get_priority_by_id(db, priority_id)
+    return priority_obj
+
+async def get_priorities_service(db:AsyncSession) -> list[Priority]:
+    priorities = await get_all_priorities(db)
+    return priorities
+
+async def update_priority_service(db:AsyncSession, priority_id:int, priority_data:PriorityCreateSchema) -> Priority | None:
+    priority_obj = await update_priority(db, priority_id, priority_data)
+    return priority_obj
+
+async def delete_priority_service(db:AsyncSession, priority_id:int) -> bool:
+    confirmation = await delete_priority(db, priority_id)
+    return confirmation
